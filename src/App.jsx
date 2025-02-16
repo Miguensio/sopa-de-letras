15f0ebd06909a481c9032d4b28a069a7a95b6807
Miguel Raaz <miguelraazbarajas@gmail.com>
2025-02-15 20:34:23 -0400

    First steps at figuring out the logic of the game array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
 import './App.css'
+import WordSearchBlock from './components/word-search-block';
 
 function App() {
 
+  const words = ["hola","polola","probando","tal"];
+  const [rows, columns] = [10, 10];
+  const size = rows * columns;
+
+  console.log(size);
+
+  function checkUp(position, rows, wordLength){
+    let wordLimit = wordLength * rows;
+    let availableSpace = position - wordLimit;
+
+    if(availableSpace > 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  const createGameArray = (words) => {
+    const gameArray = new Array(size);
+
+    for(let i = 0; i < words.length; i++){
+      let string = words[i];
+      let wordSize = string.length;
+
+      let randomPosition = Math.floor(Math.random() * size);
+
+      console.log("random position: ", randomPosition);
+
+      let positionUp = checkUp(randomPosition, rows, wordSize);
+
+      console.log("Se puede hacia arriba con la palabra ",string, ": " ,positionUp);
+
+      if(positionUp){
+        let controlPosition = 0;
+        for(let j = 0; j < wordSize; j++){
+          let startingPosition = randomPosition - controlPosition;
+          gameArray[startingPosition] = string[j];
+          controlPosition = controlPosition + rows;
+          console.log(startingPosition);
+        }
+        return console.log(gameArray);
+      }
+
+    }
+  }
+
+  createGameArray(words);
+
   return (
-    <>
+    <div className='container'>
       
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Word-search-block.jsx
@@ -0,0 +1,9 @@
+const WordSearchBlock = () => {
+    return (
+        <div className="block">
+            X
+        </div>
+    );
+}
+
+export default WordSearchBlock;
