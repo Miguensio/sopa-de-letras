0d16fc25c887af7e48f7ddab43a45ff2f6a0ca1c
Miguel Raaz <miguelraazbarajas@gmail.com>
2025-02-24 20:58:45 -0400

    Added API call to generate a word search puzzle with an inputted theme.

--- src/pages/Main-page.jsx
+++ src/pages/Main-page.jsx
@@ -1,11 +1,20 @@
 import Navbar from '../components/Nav';
 import WordSearchGrid from '../components/Word-search-grid';
+import useFetchWords from '../hooks/useFetchWordsearchWords';
 
 function Home(){
+
+  useFetchWords("vehiculos");
 
     return (
         <div className='page-container'>
           <Navbar />
+
+          <div className='input-container'>
+            <input type="text" placeholder='Introduzca una temática'/>
+            <button>Generar</button>
+          </div>
+
           <div className="game-container">
             <WordSearchGrid />
           </div>
